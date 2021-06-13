aaa4909c5d261e2514cae8598f53bcea2d5b0c16
Luis Martinez <s_k_ap3@hotmail.com>
2021-06-13 08:30:40 -0500

    habilitando la funcion de actualizar

--- src/productos/controller.js
+++ src/productos/controller.js
@@ -27,35 +27,39 @@ function readOne(req,res){
 }
 function insert(req,res){
     const data = req.body
-    data_final = []
+    let data_final = convertir_json(data)
     
-    let data_string = JSON.stringify(data)
-    data_string = data_string.replace("{","")
-    data_string = data_string.replace("}","")
-    data_string = data_string.replace(/['"]+/g, '')
-    data_string = data_string.replace(/[',]+/g, ' ')
-    data_string = data_string.split(' ')
-    
-    for(let i in data_string){
-        data_final.push(data_string[i].split(':'))
-    }
-    data_string =[] 
-    for(let i in data_final){
-        for(j in data_final[i]){
-            if(j%2!=0){
-                data_string.push(data_final[i][j])
-            }
-        }
-    }
-    data_string = data_string.toString()
-    data_string = data_string.replace(/[',]+/g, '   ')
-
     let archivo = fs.readFileSync(`${__dirname}/tabla.txt`,{encoding:"ascii"})
     fs.writeFileSync(`${__dirname}/tabla.txt`,`${archivo}
-${data_string}`)
+${data_final}`)
     response.success(req, res, "okay", 200)
 
 }
+function update(req,res){
+    const id = req.params.id
+    const data = req.body
+
+    let archivo_separado = []
+    let archivo_limpio = []
+
+    data_limpia = convertir_json(data)
+
+    let archivo = fs.readFileSync(`${__dirname}/tabla.txt`,{encoding:"ascii"})
+    archivo = archivo.split("\n")
+    for(let i in archivo){
+        archivo_separado.push(archivo[i].split("  "))
+    }
+    
+    for(let i in archivo_separado){
+        archivo_limpio.push(archivo_separado[i].filter(Boolean))
+    }
+
+    let nueva_info = archivo_limpio.filter(datos=> datos[0].indexOf(id)===-1).join("\n")
+    nueva_info+="\n"+data_limpia
+    fs.writeFileSync(`${__dirname}/tabla.txt`,nueva_info)
+    response.success(req, res, "okay", 200)
+}
+//convierte una cadena a un JSON
 function convertir_cadena(data){
     data_json=[]
     let data_separada = []
@@ -88,9 +92,35 @@ function convertir_cadena(data){
     })
     return data_json
 }
+//convierte un JSON a string
+function convertir_json(data){
+    let data_final = []
+    let data_string = JSON.stringify(data)
+    data_string = data_string.replace("{","")
+    data_string = data_string.replace("}","")
+    data_string = data_string.replace(/['"]+/g, '')
+    data_string = data_string.replace(/[',]+/g, ' ')
+    data_string = data_string.split(' ')
+    
+    for(let i in data_string){
+        data_final.push(data_string[i].split(':'))
+    }
+    data_string =[] 
+    for(let i in data_final){
+        for(j in data_final[i]){
+            if(j%2!=0){
+                data_string.push(data_final[i][j])
+            }
+        }
+    }
+    data_string = data_string.toString()
+    data_string = data_string.replace(/[',]+/g, '   ')
+    return data_string
+}
 
 module.exports = {
     readAll,
     readOne,
     insert,
+    update,
 }
--- src/productos/routes.js
+++ src/productos/routes.js
@@ -3,6 +3,7 @@ const routes = (server) => {
     server.get("/",controller.readAll)
     server.get("/:id",controller.readOne)
     server.post("/",controller.insert)
+    server.patch("/:id",controller.update)
 }
 
 
