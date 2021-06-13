82f9c7df43693ec73f09c3cc2744ebba580f20a2
Luis Martinez <s_k_ap3@hotmail.com>
2021-06-13 09:21:30 -0500

    agregando datos a la ruta

--- src/productos/routes.js
+++ src/productos/routes.js
@@ -1,9 +1,9 @@
 const controller =require("./controller")
 const routes = (server) => {
-    server.get("/",controller.readAll)
-    server.get("/:id",controller.readOne)
-    server.post("/",controller.insert)
-    server.patch("/:id",controller.update)
+    server.get("/productos",controller.readAll)
+    server.get("/productos/:id",controller.readOne)
+    server.post("/productos",controller.insert)
+    server.patch("/productos/:id",controller.update)
 }
 
 
